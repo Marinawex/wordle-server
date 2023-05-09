0be874fb2cd9695de5490e5674a364cd78a64484
marinawex <sheep803@gmail.com>
2023-05-09 20:24:14 +0300

    compression

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import * as express from 'express';
 import * as cors from 'cors';
 import { router } from './routes/wordsEndPoints';
 import * as BodyParser from 'body-parser';
+import *  as compression  from 'compression';
 
 const app = express();
 app.use(cors());
 app.use(BodyParser.json());
+app.use(compression())
 
 app.use('/', router);
 
